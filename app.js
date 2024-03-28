ab06ccde6295a9b48cd33d7f5f5e012894db7e67
Astemir Gurizhev <gurizhev.astema@mail.ru>
2024-03-28 20:46:20 +0300

    last update

--- app.js
+++ app.js
@@ -3,24 +3,34 @@ const modal = document.querySelector('.modal');
 const modalWindow = document.querySelector('.modal__window');
 const modalCloser = document.querySelector('.modal__closer');
 
+function setCounterValue(counterInput) {
+    const storedValue = localStorage.getItem('counterValue');
+    if (storedValue) {
+        counterInput.value = storedValue;
+    }
+}
+
 productCards.forEach(el => {
     
     const counterDecrease = el.querySelector('.counter__decrease');
     const counterIncrease = el.querySelector('.counter__increase');
     const counterInput = el.querySelector('.counter__input');
 
+    setCounterValue(counterInput);
+
     counterDecrease.addEventListener('click', e => {
         if(counterInput.value > 1) {
             counterInput.value--;
+            localStorage.setItem('counterValue', counterInput.value);
         }
         e.stopPropagation();
     });
     
     counterIncrease.addEventListener('click', e => {
         counterInput.value++;
+        localStorage.setItem('counterValue', counterInput.value);
         e.stopPropagation(); 
     });
-
 
     const productCardBtns = el.querySelectorAll('.product-card__add-to-cart');
 
@@ -42,17 +52,21 @@ productCards.forEach(el => {
                     const modalCounterDecrease = modalWindow.querySelector('.counter__decrease');
                     const modalCounterIncrease = modalWindow.querySelector('.counter__increase');
                     const modalCounterInput = modalWindow.querySelector('.counter__input');
+
+
+                    setCounterValue(modalCounterInput);
+
                     modalCounterDecrease.addEventListener('click', e => {
-                       
                         if (modalCounterInput.value > 1) {
                             modalCounterInput.value--;
+                            localStorage.setItem('counterValue', modalCounterInput.value);
                         }
                         e.stopPropagation(); 
                     });
                     
                     modalCounterIncrease.addEventListener('click', e => {
-                        
                         modalCounterInput.value++;
+                        localStorage.setItem('counterValue', modalCounterInput.value);
                         e.stopPropagation(); 
                     });
 
@@ -61,10 +75,10 @@ productCards.forEach(el => {
                         modalBtn.innerHTML = 'Добавить в корзину'
                         modalBtn.addEventListener('click', e => {
                             modalCounterInput.value = 1
+                            localStorage.setItem('counterValue', modalCounterInput.value);
                             alert('Товар добавлен в корзину')
                         })
                     })
-                    
                     
                 } else {
                     console.error('Ошибка: ' + xhr.status);
