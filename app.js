b496dfdc417040b6306ff237381c57e39c045d61
Astemir Gurizhev <gurizhev.astema@mail.ru>
2024-03-28 20:33:20 +0300

    update best

--- app.js
+++ app.js
@@ -3,29 +3,25 @@ const modalWindow = document.querySelector('.modal__window');
 const modal = document.querySelector('.modal');
 const modalCloser = document.querySelector('.modal__closer');
 
-
-function attachCounterHandlers(parentElement) {
-    const counterDecrease = parentElement.querySelector('.counter__decrease');
-    const counterIncrease = parentElement.querySelector('.counter__increase');
-    const counterInput = parentElement.querySelector('.counter__input');
+productCards.forEach(el => {
+    
+    const counterDecrease = el.querySelector('.counter__decrease');
+    const counterIncrease = el.querySelector('.counter__increase');
+    const counterInput = el.querySelector('.counter__input');
 
     counterDecrease.addEventListener('click', e => {
-        if (counterInput.value > 1) {
+        if(counterInput.value > 1) {
             counterInput.value--;
         }
         e.stopPropagation();
     });
-
+    
     counterIncrease.addEventListener('click', e => {
         counterInput.value++;
-        e.stopPropagation();
+        e.stopPropagation(); 
     });
-}
 
 
-attachCounterHandlers(document);
-
-productCards.forEach(el => {
     const productCardBtns = el.querySelectorAll('.product-card__add-to-cart');
 
     productCardBtns.forEach(btn => {
@@ -33,25 +29,41 @@ productCards.forEach(el => {
 
             let xhr = new XMLHttpRequest();
             xhr.open('GET', window.location.href, true);
-
+            
             xhr.onload = () => {
                 if (xhr.status >= 200 && xhr.status < 400) {
                     let response = xhr.responseText;
-                    console.log('Ответ получен: ' + response);
                     modal.classList.add('active');
 
                     modalWindow.innerHTML = el.innerHTML;
-                    console.log(el);
+                    
+                    
+                    
+                    const modalCounterDecrease = modalWindow.querySelector('.counter__decrease');
+                    const modalCounterIncrease = modalWindow.querySelector('.counter__increase');
+                    console.log(modalCounterIncrease);
+                    console.log(modalCounterDecrease);
 
-                   
-                    attachCounterHandlers(modalWindow);
-
+                    modalCounterDecrease.addEventListener('click', e => {
+                        const modalCounterInput = modalWindow.querySelector('.counter__input');
+                        if (modalCounterInput.value > 1) {
+                            modalCounterInput.value--;
+                        }
+                        e.stopPropagation(); 
+                    });
+                    
+                    modalCounterIncrease.addEventListener('click', e => {
+                        const modalCounterInput = modalWindow.querySelector('.counter__input');
+                        modalCounterInput.value++;
+                        e.stopPropagation(); 
+                    });
+                    
                 } else {
                     console.error('Ошибка: ' + xhr.status);
                 }
             };
 
-            xhr.onerror = function () {
+            xhr.onerror = function() {
                 console.error('Ошибка сети');
             };
 
